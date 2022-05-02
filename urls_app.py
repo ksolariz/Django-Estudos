233fee6a5e74091c92dcf660c624232c1fbae3a1
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-05-02 09:21:20 -0300

    Update urls_app.py

--- urls_app.py
+++ urls_app.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
     
 ]
