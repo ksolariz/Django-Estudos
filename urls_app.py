88716b12423d75420f4cbca375bba7339a9897f9
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-05-02 08:59:21 -0300

    Create urls_app.py

---
+++ urls_app.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path("<int:month>", views.monthly_challenge_number),
+    path("<str:month>", views.monthly_challenge)
+    
+]
