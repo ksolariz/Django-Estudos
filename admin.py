0449842961be59ca8315e6842a3b851ea8810739
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-05-11 16:16:28 -0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Book
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+    list_filter = ("author","rating",)
+    list_display = ("title", "author","rating",)
+admin.site.register(Book, BookAdmin)
