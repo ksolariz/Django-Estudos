810377820843473094e48f21c95e4f87c484bdcc
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-05-11 16:16:56 -0300

    Update models_book.py

--- models_book.py
+++ models_book.py
@@ -1,18 +1,29 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Author():
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True,max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", editable=False, blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])    
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
